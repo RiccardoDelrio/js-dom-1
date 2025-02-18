60903c9c9258e2d7cd1cd46fe7efb22be9a9b4ed
RiccardoDelrio <riccardodelrio92@gmail.com>
2025-02-18 15:59:22 +0100

    created a function to change two images with one click

--- main.js
+++ main.js
@@ -1,5 +1,15 @@
-const btn = document.querySelector('btn')
-const img = document.querySelector('lamp')
-btn.addEventListener(click, function () {
-
+const btn = document.querySelector('.btn')
+const img = document.querySelector('.lamp')
+let onFire = false
+btn.addEventListener("click", function () {
+    if (onFire) {
+        btn.innerText = "Accendi"
+        img.src = "./img/white_lamp.png"
+    } else {
+        btn.innerText = "Spegni"
+        img.src = "./img/yellow_lamp.png"
+    }
+    onFire = !onFire;
 })
+
+
